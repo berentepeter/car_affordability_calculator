d49eb5b7a17997da5efed48973a521bce15d1184
berentepeter <berentepeter@gmail.com>
2024-01-24 23:15:50 +0100

    day2

--- car.py
+++ car.py
@@ -1,22 +1,32 @@
 import car_properties
-
+import gas_price
 
 
 def main ():
     lst_car1 = [];
     lst_car2 = [];
 
-    brand1 = input('Select Brand for car#1:').lower();
+    while True:
+        state = input('Which state do you live in?\n').lower();
+        if state not in  gas_price.gas_prices:
+            print('Invalid state. Please select from the states of USA');
+            continue
+        else:
+            lst_car1.append(state);
+            lst_car2.append(state);
+            break
+        
+    brand1 = input('Select Brand for car#1:\n').lower();
     lst_car1.append(brand1);
 
-    type1 = input('Select Type for car#1: ').lower();
+    type1 = input('Select Type for car#1:\n').lower();
     lst_car1.append(type1);
 
     ##colour1 = input('Select Colour for car#1:').lower();
     ##lst_car1.append(colour1)
 
     while True:
-        colour1 = input('Enter your colour: ').lower();
+        colour1 = input('Enter your colour:\n').lower();
 
         if colour1 not in  car_properties.car_colours:
             print('This is not an available colour, please pick another');
@@ -28,7 +38,8 @@ def main ():
 
     print(lst_car1)
    # car2 = input('Enter a car option2(brand, type, colour): ');
-    print('You Entered:{} and {}'.format(lst_car1, lst_car2));
+    print('You selected a {} {} in {}'.format(*lst_car1));
+    #print('You also selected a {} {} in {}'.format(*lst_car2));
 
 if __name__ == '__main__':
     main ()
--- car_properties.py
+++ car_properties.py
@@ -13,3 +13,56 @@ car_colours = [
     'purple',
     'pink'
 ]
+
+car_types = [
+    'Sedan',
+    'SUV',
+    'Hatchback',
+    'Coupe',
+    'Convertible/ Roadster',
+    'Minivan',
+    'Pickup Truck',
+    'Crossover',
+    'Sports',
+    'Electric',
+    'Hybrid',
+    'Luxury',
+    'Compact',
+    'Wagon',
+    '4x4'
+]
+
+car_consumption = {
+    'low consumption' : 1,
+    'mid consumption' : 2,
+    'high consumption' : 3,
+    'too much consumption' : 4
+}
+
+car_comfort = {
+    'low comfort' : 1,
+    'average comfort' : 2,
+    'very much comfort' : 3,
+    'extra comfort' : 4
+}
+
+car_seats = (2,4,5,7,8)
+
+car_types = {
+    'Sedan' : 4,
+    'SUV' : 7,
+    'Hatchback' : 5,
+    'Coupe' : 2,
+    'Convertible/ Roadster' : 4,
+    'Minivan' : 8,
+    'Pickup Truck' : 2,
+    'Crossover' : 5,
+    'Sports' : 2,
+    'Electric' : 4,
+    'Hybrid' : 5,
+    'Luxury' : 4,
+    'Compact' : 4,
+    'Wagon' : 7,
+    '4x4' : 4
+}
+
