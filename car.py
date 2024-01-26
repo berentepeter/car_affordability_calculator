2cb3539bdf12df7094a23f0ad92f50ed6dca179c
berentepeter <berentepeter@gmail.com>
2024-01-26 23:32:49 +0100

    day3

--- car.py
+++ car.py
@@ -1,5 +1,30 @@
 import car_properties
 import gas_price
+import states
+import math
+
+def monthly_loan_payment(principal, interest_rate, term):
+    # loan amortization formula: M = P⋅r⋅(1+r)^n / (1+r)^n−1 
+    # M is the monthly payment,
+    # P is the remaining loan principal,
+    # r is the monthly interest rate (annual interest rate divided by 12), and
+    # n is the total number of payments.
+    monthly_interest_rate = interest_rate / 12;
+    return (principal * monthly_interest_rate * (1 + monthly_interest_rate) ** term) / ((1 + monthly_interest_rate) ** term - 1);
+    
+
+def monthly_maintenance_cost(yearly_maintenance_budget):
+    return yearly_maintenance_budget / 12;
+
+def monthly_gas_price(car_usage, fuel_consumption, state):
+    gas = gas_price.gas_prices[state]
+    print(gas)
+    return ((car_usage * 30) / fuel_consumption) * gas;
+
+
+def monthly_total_payment():
+    pass
+
 
 
 def main ():
@@ -8,25 +33,42 @@ def main ():
 
     while True:
         state = input('Which state do you live in?\n').lower();
-        if state not in  gas_price.gas_prices:
+        if state not in states.states:
             print('Invalid state. Please select from the states of USA');
             continue
         else:
             lst_car1.append(state);
             lst_car2.append(state);
             break
-        
-    brand1 = input('Select Brand for car#1:\n').lower();
+    
+    print("INFO: Gas price in {} is ${}".format(state.title(), gas_price.gas_prices[state]))
+
+    loan_principal = int(input('\nPlease enter the principal amount of the loan:\n'));
+
+    loan_interest_rate = float(input('\nPlease enter the yearly interest rate of the loan (i.e: if the interest is 3.2% then enter 3.2):\n'));
+    try:
+        1/loan_interest_rate;
+        1/(loan_interest_rate + math.sqrt(loan_interest_rate ** 2));
+    except:
+        print("ERROR: please try again with interest rate being a positive number greater than 0");
+        exit();
+    finally:
+        loan_interest_rate * 0.01;
+
+    loan_term = int(input('\nPlease enter the loan term (amount of months):\n'));
+
+    maintenance_budget = int(input('\nEnter yearly budget for car maintenance:\n'));
+    
+    print(monthly_loan_payment(loan_principal, loan_interest_rate, loan_term))
+
+    brand1 = input('\nSelect Brand for car#1:\n').lower();
     lst_car1.append(brand1);
 
-    type1 = input('Select Type for car#1:\n').lower();
+    type1 = input('\nSelect Type for car#1:\n').lower();
     lst_car1.append(type1);
 
-    ##colour1 = input('Select Colour for car#1:').lower();
-    ##lst_car1.append(colour1)
-
     while True:
-        colour1 = input('Enter your colour:\n').lower();
+        colour1 = input('\nEnter your colour:\n').lower();
 
         if colour1 not in  car_properties.car_colours:
             print('This is not an available colour, please pick another');
@@ -35,7 +77,6 @@ def main ():
             lst_car1.append(colour1);
             break
 
-
     print(lst_car1)
    # car2 = input('Enter a car option2(brand, type, colour): ');
     print('You selected a {} {} in {}'.format(*lst_car1));
@@ -43,3 +84,4 @@ def main ():
 
 if __name__ == '__main__':
     main ()
+
--- car_properties.py
+++ car_properties.py
@@ -14,23 +14,83 @@ car_colours = [
     'pink'
 ]
 
-car_types = [
-    'Sedan',
-    'SUV',
-    'Hatchback',
-    'Coupe',
-    'Convertible/ Roadster',
-    'Minivan',
-    'Pickup Truck',
-    'Crossover',
-    'Sports',
-    'Electric',
-    'Hybrid',
-    'Luxury',
-    'Compact',
-    'Wagon',
-    '4x4'
-]
+car_types = {
+    'Sedan' : {
+        'seats' : 4,
+        'consumption' : 1,
+        'comfort' : 2
+    },
+    'SUV': {
+        'seats' : 4,
+        'consumption' : 2,
+        'comfort' : 3
+    },
+    'Hatchback': {
+        'seats' : 4,
+        'consumption' : 2,
+        'comfort' : 3
+    },
+    'Coupe': {
+        'seats' : 4,
+        'consumption' : 2,
+        'comfort' : 2
+    },
+    'Convertible/ Roadster': {
+        'seats' : 4,
+        'consumption' : 3,
+        'comfort' : 2
+    },
+    'Minivan': {
+        'seats' : 4,
+        'consumption' : 2,
+        'comfort' : 3
+    },
+    'Pickup Truck': {
+        'seats' : 4,
+        'consumption' : 3,
+        'comfort' : 1
+    },
+    'Crossover': {
+        'seats' : 4,
+        'consumption' : 3,
+        'comfort' : 2
+    },
+    'Sports': {
+        'seats' : 4,
+        'consumption' : 4,
+        'comfort' : 1
+    },
+    'Electric': {
+        'seats' : 4,
+        'consumption' : 0,
+        'comfort' : 3
+    },
+    'Hybrid': {
+        'seats' : 4,
+        'consumption' : 1,
+        'comfort' : 3
+    },
+    'Luxury': {
+        'seats' : 4,
+        'consumption' : 3,
+        'comfort' : 4
+    },
+    'Compact': {
+        'seats' : 4,
+        'consumption' : 2,
+        'comfort' : 3
+    },
+    'Wagon': {
+        'seats' : 4,
+        'consumption' : 2,
+        'comfort' : 3
+    },
+    '4x4': {
+        'seats' : 4,
+        'consumption' : 3,
+        'comfort' : 2
+    }
+}
 
 car_consumption = {
     'low consumption' : 1,
@@ -46,23 +106,83 @@ car_comfort = {
     'extra comfort' : 4
 }
 
-car_seats = (2,4,5,7,8)
 
-car_types = {
-    'Sedan' : 4,
-    'SUV' : 7,
-    'Hatchback' : 5,
-    'Coupe' : 2,
-    'Convertible/ Roadster' : 4,
-    'Minivan' : 8,
-    'Pickup Truck' : 2,
-    'Crossover' : 5,
-    'Sports' : 2,
-    'Electric' : 4,
-    'Hybrid' : 5,
-    'Luxury' : 4,
-    'Compact' : 4,
-    'Wagon' : 7,
-    '4x4' : 4
+car_brands = {
+    'mercedes-benz' : {
+        'price' : 108000,
+        'maintenance' : 3000
+    },
+    'audi' : {
+        'price' : 99000,
+        'maintenance' : 2900
+    },
+    'bmw': {
+        'price' : 92000,
+        'maintenance' : 2800
+    },
+    'lexus': {
+        'price' : 90000,
+        'maintenance' : 2700
+    },
+    'cadillac': {
+        'price' : 80000,
+        'maintenance' : 2600
+    },
+    'infiniti': {
+        'price' : 75000,
+        'maintenance' : 2500
+    },
+    'acura' : {
+        'price' : 70000,
+        'maintenance' : 2400
+    },
+    'volkswagen': {
+        'price' : 60000,
+        'maintenance' : 2300
+    },
+    'kia': {
+        'price' : 50000,
+        'maintenance' : 1000
+    },
+    'subaru': {
+        'price' : 50000,
+        'maintenance' : 1600
+    },
+    'toyota': {
+        'price' : 50000,
+        'maintenance' : 1300
+    },
+    'nissan': {
+        'price' : 40000,
+        'maintenance' : 1500
+    },
+    'hyundai': {
+        'price' : 37000,
+        'maintenance' : 1100
+    },
+    'mazda': {
+        'price' : 35000,
+        'maintenance' : 1400
+    },
+    'honda': {
+        'price' : 35000,
+        'maintenance' : 1200
+    },
+    'jeep': {
+        'price' : 30000,
+        'maintenance' : 1700
+    },
+    'gmc': {
+        'price' : 25000,
+        'maintenance' : 1800
+    },
+    'ram': {
+        'price' : 23000,
+        'maintenance' : 2000
+    },
+    'ford': {
+        'price' : 20000,
+        'maintenance' : 1900
+    }
 }
 
